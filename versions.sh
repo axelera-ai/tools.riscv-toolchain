baf736f8037659e25c4fc3f0f8e6023a931eacc7
Florian Zaruba <florian.zaruba@axelera.ai>
2024-10-08 10:20:58 +0200

    [ci] Make package name generic

--- versions.sh
+++ versions.sh
@@ -6,4 +6,4 @@ GCC_BRANCH="releases/gcc-14"
 NEWLIB_BRANCH="newlib-4.4.0"
 LLVM_BRANCH="axelera-ai-18.1.8"
 PKG_DEPENDS="libc6, libstdc++6, libgcc-s1, libgmp10, libmpfr6, libexpat1, libtinfo6, libncursesw6, zlib1g, libmpc3"
-PKG_RELEASE_NUMBER=0
+PKG_RELEASE_NUMBER=1
